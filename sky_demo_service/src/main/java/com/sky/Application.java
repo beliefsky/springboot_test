ffb3c456ae4a4e09c1c22f27411d2b0bbcbd30e7
weijing <ibeliefsky@gmail.com>
2018-04-17 18:10:06 +0800

    init

---
+++ sky_demo_service/src/main/java/com/sky/Application.java
@@ -0,0 +1,17 @@
+package com.sky;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@MapperScan("com.sky.**.dao")
+@EnableTransactionManagement
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
